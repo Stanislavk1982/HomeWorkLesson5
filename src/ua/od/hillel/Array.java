b38369a1ae3ec022ac427339b324a54a823f6e00
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-04-05 23:05:53 +0300

    Commit 4 HomeWork 5.1

--- src/ua/od/hillel/Array.java
+++ src/ua/od/hillel/Array.java
@@ -5,6 +5,22 @@ package ua.od.hillel;
  */
 public class Array {
     public static void main(String[] args) {
+        int[] array = {21, 1, 12, 25, 95, 22, 45, 8, 3, 111};
+        int tempNumber = 0, counter = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    tempNumber = array[j];
+                    array[j] = array[i];
+                    array[i] = tempNumber;
+                    counter++;
+                }
+            }
 
+
+        }
+        Utils.printArray(array);
+        System.out.println(counter);
     }
+
 }
--- src/ua/od/hillel/HW_2_2.java
+++ src/ua/od/hillel/HW_2_2.java
@@ -6,7 +6,7 @@ package ua.od.hillel;
 public class HW_2_2 {
     public static void main(String[] args) {
         int[] array1 = {1, 3, 5, 6, 7, 10, 11};
-        int[] array2 = {2, 4, 8, 9, 12, 14};
+        int[] array2 = {2, 4, 8, 9, 12, 14,20};
         int x = 0, y = 0, length = array1.length + array2.length;
         int[] array3 = new int[length];
         for (int i = 0; i < length; i++) {
