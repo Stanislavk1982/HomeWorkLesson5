c26f79e41ab71db9965f76948f64eda7d2136f73
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-04-06 00:14:50 +0300

    commit9

--- src/ua/od/hillel/Array.java
+++ src/ua/od/hillel/Array.java
@@ -5,11 +5,11 @@ package ua.od.hillel;
  */
 public class Array {
     public static void main(String[] args) {
-        int[] array = {2, 6, 3, 12, 7, 4, 1, 5,88,9};
+        int[] array = {2, 6, 3, 12, 7, 4, 1, 5, 88, 9};
         int i, j, temp, tempIndex = -1, counter = 0, min;
         for (i = 0; i < array.length - 1; i++) {
             min = array[i];
-            tempIndex=-1;
+            tempIndex = -1;
             for (j = i + 1; j < array.length; j++) {
                 if (min > array[j]) {
                     min = array[j];
@@ -17,10 +17,11 @@ public class Array {
                     counter++;
                 }
             }
-            if (tempIndex !=-1){
-            temp = array[i];
-            array[i] = array[tempIndex];
-            array[tempIndex] = temp;}
+            if (tempIndex != -1) {
+                temp = array[i];
+                array[i] = array[tempIndex];
+                array[tempIndex] = temp;
+            }
 
         }
 
@@ -28,5 +29,4 @@ public class Array {
         System.out.println(counter);
     }
 
-    
 }
